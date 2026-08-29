7b249e1005567e19041cc8f1770e02047ffee369
Dimoraga <di.moragah@duocuc.cl>
2026-08-29 18:23:24 -0400

    primer commit

---
+++ src/main/java/com/duoc/sumativauno/sumativauno/SumativaunoApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.sumativauno.sumativauno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumativaunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumativaunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/sumativauno/sumativauno/SumativaunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.sumativauno.sumativauno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumativaunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
